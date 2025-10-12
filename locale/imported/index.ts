ccd2f89deee0da8a8d2eb6617adbe5c4ae5aa2bf
Allen <stuallen@gmail.com>
2025-10-13 01:29:29 +0800

    chore: 測試資料、ts型別檢查指令

---
+++ locale/imported/index.ts
@@ -0,0 +1,19 @@
+export const foo = 'foo';
+
+export const objData = {
+    a: 'a',
+    b: 'b',
+    c: 'c',
+    d: 'd',
+    e: 'e',
+    f: 'f',
+    g: 'g',
+    h: 'h',
+    i: 'i',
+    j: 'j',
+}
+
+export default {
+    foo,
+    objData,
+}
