04f13edd6a0af110698557dfba9fd5351bf66d80
Allen <stuallen@gmail.com>
2025-10-19 16:30:34 +0800

    chore: eslint-plugin-unused-imports

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default defineConfig([
   ...tseslint.configs.recommended,
   {
+    plugins: {
+      "unused-imports": unusedImports,
+    }
+  },
+  {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      semi: ["error", "always"]
+      semi: ["error", "always"],
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+          "warn",
+          {
+              "vars": "all",
+              "varsIgnorePattern": "^_",
+              "args": "after-used",
+              "argsIgnorePattern": "^_",
+          },
+      ]
     },
   },
 ]);
