7b1a7a05f97f8ce29c55f47f18b59724f6411bc3
Allen <stuallen@gmail.com>
2026-02-07 01:20:40 +0800

    docs: package.json, tsup.config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/index.ts', 'src/bin/i18n-check.ts'],
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    dts: true,
-    format: ['cjs', 'esm'],
-    external: [/^@babel\//],
-    // banner: {
-    //   js: '#!/usr/bin/env node'
-    // }
-  });
+  entry: ['src/index.ts', 'src/bin/i18n-check.ts'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  format: ['esm'],
+  external: [/^@babel\//],
+  // banner: {
+  //   js: '#!/usr/bin/env node'
+  // }
+});
