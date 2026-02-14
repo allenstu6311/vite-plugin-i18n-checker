f42eb3544dc6dfe113c9ec42d46889b6adaaa498
Allen <stuallen@gmail.com>
2026-02-14 20:46:08 +0800

    fix: cli report指令修復

--- src/bin/i18n-check.ts
+++ src/bin/i18n-check.ts
@@ -1,55 +1,55 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import { resolve } from 'path';
-import { pathToFileURL } from 'url';
-import { createServer } from 'vite';
-import i18nCheckerPlugin from '..';
+import { program } from "commander";
+import { resolve } from "path";
+import { pathToFileURL } from "url";
+import { createServer } from "vite";
+import i18nCheckerPlugin from "..";
 
 program
-    .option('-s, --sourceLocale <sourceLocale>')
-    .option('-p, --localesPath <localesPath>')
-    .option('-x, --extensions <extensions>')
-    .option('-f, --failOnError')
-    .option('-m, --applyMode <applyMode>')
-    .option('--include <patterns...>')
-    .option('-e, --exclude <patterns...>')
-    .option('-i, --ignoreKeys <patterns...>')
-    .option('-r, --rules <path>')
-    .option('-w, --watch')
-    .option('--report-dir <path>')
-    .option('--report-retention <days>')
-    .option('--override')
-    .option('--autoFill')
-    .option('--autoDelete')
-    .option('--no-preview')
-    .parse();
-
+  .option("-s, --sourceLocale <sourceLocale>")
+  .option("-p, --localesPath <localesPath>")
+  .option("-x, --extensions <extensions>")
+  .option("-f, --failOnError")
+  .option("-m, --applyMode <applyMode>")
+  .option("--include <patterns...>")
+  .option("-e, --exclude <patterns...>")
+  .option("-i, --ignoreKeys <patterns...>")
+  .option("-r, --rules <path>")
+  .option("-w, --watch")
+  .option("--report-dir <path>")
+  .option("--report-retention <days>")
+  .parse();
 
 async function loadModule(path: string) {
-    const absPath = resolve(process.cwd(), path);
-    const mod = await import(pathToFileURL(absPath).href);
-    return mod?.default ?? mod;
+  const absPath = resolve(process.cwd(), path);
+  const mod = await import(pathToFileURL(absPath).href);
+  return mod?.default ?? mod;
 }
 
 async function run() {
-    const opts: any = program.opts();
-    const {
-        watch,
-    } = opts;
+  const opts: any = program.opts();
+  const { watch, reportDir, reportRetention } = opts;
 
-    if (opts.rules) {
-        const fileRules = await loadModule(opts.rules);
-        opts.rules = fileRules;
-    }
-    const server = await createServer({
-        root: process.cwd(),
-        plugins: [i18nCheckerPlugin(opts)]
-    });
-    await server.pluginContainer.buildStart({}); // 讓Vite初始化模組系統
-    if (!watch || process.env.CI) await server.close();
+  opts.report = {
+    ...opts.report,
+    dir: reportDir || 'i18CheckerReport',
+    retention: reportRetention ?? 7,
+  };
+
+  if (opts.rules) {
+    const fileRules = await loadModule(opts.rules);
+    opts.rules = fileRules;
+  }
+
+  const server = await createServer({
+    root: process.cwd(),
+    plugins: [i18nCheckerPlugin(opts)],
+  });
+  await server.pluginContainer.buildStart({}); // 讓Vite初始化模組系統
+  if (!watch || process.env.CI) await server.close();
 }
 
-run().catch(err => {
-    console.error('[i18n-check] Error:', err.message);
-    process.exit(1);
+run().catch((err) => {
+  console.error("[i18n-check] Error:", err.message);
+  process.exit(1);
 });
