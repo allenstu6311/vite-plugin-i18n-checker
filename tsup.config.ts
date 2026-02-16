380f3f1022e63879bbc9f58e07ff367f7f1ce4f3
Allen <stuallen@gmail.com>
2026-02-16 23:17:25 +0800

    docs: tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,29 +1,14 @@
 import { defineConfig } from 'tsup';
 
-const shared = {
+export default defineConfig({
+  entry: ['src/index.ts', 'src/bin/i18n-check.ts'],
   splitting: false,
   sourcemap: false,
+  clean: true,
+  dts: true,
+  format: ['esm', 'cjs'],
   external: [/^@babel\//],
   env: {
     NODE_ENV: 'production',
   },
-};
-
-export default defineConfig([
-  {
-    ...shared,
-    entry: ['src/index.ts'],
-    clean: true,
-    dts: true,
-    format: ['esm', 'cjs'],
-  },
-  {
-    ...shared,
-    entry: ['src/bin/i18n-check.ts'],
-    dts: false,
-    format: ['esm'],
-    banner: {
-      js: '#!/usr/bin/env node',
-    },
-  },
-]);
+});
